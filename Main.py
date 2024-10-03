646834227cc9b71a875cb15100149c9ef7f0017b
izlude83 <izlude83@hotmail.com>
2024-10-03 23:58:52 +0800

    Added password protection

--- Main.py
+++ Main.py
@@ -1,7 +1,11 @@
 # Set up and run this Streamlit App
 import streamlit as st
 from functions import worker1 
+from functions.utility import check_password
 
+# Check if the password is correct.  
+if not check_password():  
+    st.stop()
 
 # region <--------- Streamlit App Configuration --------->
 st.set_page_config(
